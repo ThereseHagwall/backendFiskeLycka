009d262b441b3ec63f195ba5801d2790bf0c7478
Therese Hagwall <theresejager@live.se>
2024-05-19 18:56:09 +0200

    Add getUser

---
+++ pages/api/users/getUser.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Hanterare för att hämta alla användare
+export default async function getUsers(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        try {
+            const users = await prisma.user.findMany();
+            res.status(200).json(users);
+        } catch (error: any) {
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        res.status(405).json({ error: 'Vi stöder endast GET-förfrågningar' });
+    }
+}
