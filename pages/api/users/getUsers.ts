d8c776f27a84888c819d18aa43679d67d6fb6309
Therese Hagwall <theresejager@live.se>
2024-05-28 10:39:06 +0200

    changing get all users info.

--- pages/api/users/getUsers.ts
+++ pages/api/users/getUsers.ts
@@ -7,7 +7,15 @@ const prisma = new PrismaClient();
 export default async function getUsers(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
-            const users = await prisma.user.findMany();
+            const users = await prisma.user.findMany({
+                select: {
+                    id: true,
+                    email: true,
+                    name: true,
+                    admin: true,
+                    posts: true,
+                },
+            });
             res.status(200).json(users);
         } catch (error: any) {
             res.status(500).json({ error: error.message });
