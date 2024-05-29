3779caea63848c01cde4890bc5869592367e530c
Felix Anneblad <felix.anneblad@gmail.com>
2024-05-29 15:44:57 +0200

    feat: Add API endpoints for getting posts and catch reports

---
+++ pages/api/posts/getCatchReports.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function getposts(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        try {
+            const users = await prisma.catchReport.findMany({
+                select: {
+                    id: true,
+                    date: true,
+                    location: true,
+                    weather: true,
+                    waterTemp: true,
+                    species: true,
+                    weight: true,
+                    length: true,
+                    bait: true,
+                    method: true,
+                    notes: true,
+                    image: true,
+                    author: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                },
+            });
+            res.status(200).json(users);
+        } catch (error: any) {
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        res.status(405).json({ error: 'Vi stöder endast GET-förfrågningar' });
+    }
+}
