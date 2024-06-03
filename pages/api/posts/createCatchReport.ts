a825a69e54d7a0a8322d78ec6bf1b5f5db2df926
Felix Anneblad <felix.anneblad@gmail.com>
2024-06-03 14:07:06 +0200

    chore: fix image with base64 in both create and get

--- pages/api/posts/createCatchReport.ts
+++ pages/api/posts/createCatchReport.ts
@@ -3,23 +3,31 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function createCatchReport( req: NextApiRequest, res: NextApiResponse ) {
+export default async function createCatchReport(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     try {
       const { catchReport } = req.body;
 
-        await prisma.catchReport.create({
+      const imageBuffer = catchReport.image
+        ? Buffer.from(catchReport.image, "base64")
+        : undefined;
+
+      await prisma.catchReport.create({
         data: {
           ...catchReport,
+          image: imageBuffer,
         },
       });
 
       res.status(201).json({ message: "Catch report created" });
     } catch (error) {
+      console.error("Error creating catch report:", error);
       res.status(500).json({ error: "Unable to create catch report" });
     }
   } else {
-    // Handle any other HTTP method
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
